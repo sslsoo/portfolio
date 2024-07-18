1149d2fe69c490507255dcf42d7c458cc666c7c5
jinsoojeon2 <pray@chosunbiz.com>
2024-07-18 10:32:32 +0900

    이력서 다운 배포

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,84 @@
-import React from "react";
+'use client';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({ weight: '400', subsets: ['latin'] });
 
 const Home: React.FC = () => {
+  useEffect(() => {
+    const text = document.querySelector('.text-animation');
+    if (text) {
+      text.innerHTML =
+        text.textContent
+          ?.split('')
+          .map((char, i) => {
+            if (char === ' ') {
+              return `<span class="text-color-animation char space" style="animation-delay: ${
+                i * 0.1
+              }s">&nbsp;</span>`;
+            }
+            return `<span class="text-color-animation char" style="animation-delay: ${
+              i * 0.1
+            }s">${char}</span>`;
+          })
+          .join('') || '';
+
+      const chars = document.querySelectorAll('.char');
+      chars.forEach((char, i) => {
+        char.classList.add('animate-char');
+        char.style.animationDelay = `${i * 0.1}s`;
+      });
+    }
+  }, []);
+
   return (
     <div>
       <Header />
       <main>
-        <p>Welcome to my Next.js app!</p>
+        <div className="container">
+          <section className="mx-auto mt-20">
+            <div className="flex">
+              <div>
+              <div className="text-5xl mb-4">안녕하세요.</div>
+        <div className="text-5xl mb-4">
+          <span className="text-animation text-color-animation">프론트엔드 개발자</span>
+        </div>
+        <div className="text-5xl mb-12">전진수입니다.</div>
+        <div className={`text-lg font-bold mb-4 ${poppins.className}`}>
+          소통하는 개발자
+          <br />
+          <blockquote className="text-lg text-gray-500 border-l-4 border-gray-300 pl-4 italic mt-4">
+            다양한 경험을 바탕으로 조금 더 성장하고 싶은 프론트엔드 개발자입니다.
+          </blockquote>
+        </div>
+                <a
+            href="/resume.pdf"
+            download="resume.pdf"
+            className="mt-8 inline-block w-40 px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition-colors duration-300 flex items-center justify-center space-x-2"
+          >
+            <span>이력서</span>
+            <svg
+              stroke="currentColor"
+              fill="none"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+              <polyline points="7 10 12 15 17 10"></polyline>
+              <line x1="12" y1="15" x2="12" y2="3"></line>
+            </svg>
+          </a>
+              </div>
+              <div>🧑🏻‍💻</div>
+            </div>
+          </section>
+        </div>
       </main>
     </div>
   );
